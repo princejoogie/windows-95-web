aeeca9b933f13a13ce50342a7166eb6ea2c5dd86
princejoogie <princejoogie@gmail.com>
2021-06-14 05:22:01 +0800

    cleaned

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,8 @@
 import React from "react";
-import { binEmptyIcon, myComputerIcon } from "./assets";
-import { Window, Footer, DesktopItem } from "./components";
+import { Win95 } from "./pages";
 
 const app: React.FC = () => {
-  return (
-    <div className="flex flex-col w-full h-screen overflow-hidden">
-      <div className="relative flex-1 p-1 bg-primary">
-        <DesktopItem name="My Computer" icon={myComputerIcon}>
-          {({ visible, setVisible }) => {
-            return (
-              <Window
-                {...{
-                  visible,
-                  setVisible,
-                  windowTitle: "My Computer",
-                }}
-              />
-            );
-          }}
-        </DesktopItem>
-
-        <DesktopItem name="Recycle Bin" icon={binEmptyIcon}>
-          {({ visible, setVisible }) => {
-            return (
-              <Window
-                {...{
-                  visible,
-                  setVisible,
-                  windowTitle: "Recycle Bin",
-                }}
-              />
-            );
-          }}
-        </DesktopItem>
-      </div>
-      <Footer />
-    </div>
-  );
+  return <Win95 />;
 };
 
 export default app;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,2 @@
 export { default as Aos } from "./aos";
+export { default as Win95 } from "./win-95";
